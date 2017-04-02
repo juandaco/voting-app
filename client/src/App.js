b616b88579bf756d778c83d2b6eaf927303c4130
Juan D. Acosta <juandacorias@gmail.com>
2017-04-02 17:25:00 -0500

    Minor Fixes

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ import PopUpDialog from './components/PopUpDialog';
 import ApiCalls from './ApiCalls';
 import config from './config';
 
-
 class App extends Component {
   constructor() {
     super();
